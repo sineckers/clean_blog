913236c968c8fb14b25a2630d50dd24cc48471ff
SinecKers <yasar.ylmz.yt@gmail.com>
2021-05-28 23:03:49 +0300

    odev8

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  const blog = {
-    id: 1,
-    name: 'Blog Name',
-    description: 'Blog Description',
-  };
-  res.send(blog);
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
 });
 
 const PORT = 3000;
+
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}.`);
 });
